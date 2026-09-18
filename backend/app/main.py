6bf7cf9a4bd302e04abed1573f930fa9d1468849
thewalker_045 <2024cs_diwakarsharmaaditya_a@nie.ac.in>
2026-09-18 21:43:38 +0530

    added resend for email otp verification

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+import resend
+import os
 from app.routes.auths import router as auth_router
 
+resend.api_key = os.getenv("RESEND_API_KEY")
 app = FastAPI()
 
 app.include_router(auth_router)
@@ -10,4 +13,21 @@ app.include_router(auth_router)
 def home():
     return {
         "message": "Nyaay Sakha API is running"
+    }
+
+@app.post("/send-email")
+def send_email():
+
+    params = {
+        "from": "onboarding@resend.dev",
+        "to": ["your_email@gmail.com"],
+        "subject": "Nyaay Sakha Test",
+        "html": "<h2>Hello from Nyaay Sakha!</h2><p>Resend is working.</p>"
+    }
+
+    email = resend.Emails.send(params)
+
+    return {
+        "message": "Email sent",
+        "email": str(email)
     }
